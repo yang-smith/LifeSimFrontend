5c31708f436205ff187668387cb9701d2830c6f5
yang <zy892065502@gmail.com>
2023-09-22 19:38:33 +0800

    change to https

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,7 @@ export default {
   data() {
     return {
       player: {},
-      baseURL: 'http://lifeapi.autumnriver.chat/api',
+      baseURL: 'https://lifeapi.autumnriver.chat/api',
       eventDescription: "",
       update:"",
       gameOver: false,
