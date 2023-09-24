5070847f822177ac6270a94bbf513fc2443deadf
yang <zy892065502@gmail.com>
2023-09-24 11:09:24 +0800

    change age add

--- src/App.vue
+++ src/App.vue
@@ -177,7 +177,7 @@ export default {
       this.eventDescription += this.update;
       this.isChoiceLoading = false;
       this.continueOrchoice = true;
-      this.player.age += 10;
+      this.player.age += 50;
       this.preloadNextEvent();  // 处理用户选择后预加载下一个事件
     },
     resetGame() {
